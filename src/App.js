b76ef148bed37bea38bd0284fd6d5610c187cea6
Mehul <318097@gmail.com>
2019-12-13 18:06:04 +0530

    Add: Paper CSS, Routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+
+import Home from './components/Home';
+import Results from './components/Results';
+import NotFound from './components/NotFound';
 
 const App = () => {
   return (
     <div className="app">
-      <button>Default</button>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/home" exact component={Home} />
+          <Route path="/results" exact component={Results} />
+          <Route path="/" exact render={() => <Redirect to="/home" />} />
+          <Route component={NotFound} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
